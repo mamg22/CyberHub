42959a9f41fbb9c41554a5199d7f2557014726b7
mamg22 <45301823+mamg22@users.noreply.github.com>
2022-12-28 20:34:29 -0400

    Mover navbar a libs

--- cyber/menu-principal.php
+++ cyber/menu-principal.php
@@ -16,7 +16,7 @@ safe_session_start();
 </head>
 
 <body>
-    <?php require('navbar.php') ?>
+    <?php require('libs/navbar.php') ?>
     <h1 class="titulo">Sistema de inventario<br>Cyber Rodríguez</h1>
     <h2 class="titulo">Bienvenido, <?= $_SESSION['nombre'] ?></h2>
     <br>
